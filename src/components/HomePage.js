5e142848aa7b3c62dfa3e399b1d84821a3af54e0
Michael McMasters <jmcmasters411@gmail.com>
2021-12-12 13:52:12 -0500

    Add test and prod email to send to

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+const TEST_EMAIL = "RealKatieRealEstate@gmail.com";
+const PROD_EMAIL = "Kate.mcmasters@foxroach.com"
+
+
 const HomePage = () => {
   return (
-    <form action="https://formsubmit.co/katie.mcmasters@foxroach.com" method="POST">
-      <input placeholder='First Name' type="text" name="name" required />
-      <input placeholder='Last Name' type="text" name="name" required />
+    <form action={`https://formsubmit.co/${TEST_EMAIL}`} method="POST">
+      <input placeholder='First Name' type="text" name="first-name" required />
+      <input placeholder='Last Name' type="text" name="last-name" required />
       <input placeholder='Phone Number' type="tel" name="tel" required />
       <button type="submit">Accept</button>
     </form>
