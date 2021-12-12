5525450578109952dd8ddc47ada2e29871856bdb
Michael McMasters <jmcmasters411@gmail.com>
2021-12-12 18:08:13 -0500

    Console.log env variable

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -8,13 +8,14 @@ console.log(email);
 
 const HomePage = () => {
   return (
-    <form action={`https://formsubmit.co/${email}`} method="POST">
-      <input placeholder='First Name' type="text" name="first-name" required />
-      <input placeholder='Last Name' type="text" name="last-name" required />
-      <input placeholder='Phone Number' type="tel" name="tel" required />
-      <input placeholder='Email Address' type="email" name="email" required />
-      <button type="submit">Accept</button>
-    </form>
+    // <form action={`https://formsubmit.co/${email}`} method="POST">
+    //   <input placeholder='First Name' type="text" name="first-name" required />
+    //   <input placeholder='Last Name' type="text" name="last-name" required />
+    //   <input placeholder='Phone Number' type="tel" name="tel" required />
+    //   <input placeholder='Email Address' type="email" name="email" required />
+    //   <button type="submit">Accept</button>
+    // </form>
+      <button onClick={() => console.log(email)}>HI</button>
   );
 };
 
