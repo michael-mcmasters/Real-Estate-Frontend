bece040acf86e2f9fd56b3ae0d5915d11ac70776
Michael McMasters <jmcmasters411@gmail.com>
2021-12-12 18:18:38 -0500

    Clean up code after debugging env vars

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,22 +1,16 @@
 import React from 'react';
 
-// const TEST_EMAIL = "RealKatieRealEstate@gmail.com";
-// const PROD_EMAIL = "Kate.mcmasters@foxroach.com"
-
-const email = process.env.REACT_APP_EMAIL_TO_SEND_TO || "no email assigned";
-console.log(email);
-//
+const email = process.env.REACT_APP_EMAIL_TO_SEND_TO;
 
 const HomePage = () => {
   return (
-    // <form action={`https://formsubmit.co/${email}`} method="POST">
-    //   <input placeholder='First Name' type="text" name="first-name" required />
-    //   <input placeholder='Last Name' type="text" name="last-name" required />
-    //   <input placeholder='Phone Number' type="tel" name="tel" required />
-    //   <input placeholder='Email Address' type="email" name="email" required />
-    //   <button type="submit">Accept</button>
-    // </form>
-      <button onClick={() => console.log(email)}>HI</button>
+    <form action={`https://formsubmit.co/${email}`} method="POST">
+      <input placeholder='First Name' type="text" name="first-name" required />
+      <input placeholder='Last Name' type="text" name="last-name" required />
+      <input placeholder='Phone Number' type="tel" name="tel" required />
+      <input placeholder='Email Address' type="email" name="email" required />
+      <button type="submit">Accept</button>
+    </form>
   );
 };
 
