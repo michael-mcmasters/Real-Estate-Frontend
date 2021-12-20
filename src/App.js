5449d9ac827403cd3fad608694f0f68007fc5258
Michael McMasters <jmcmasters411@gmail.com>
2021-12-19 20:20:32 -0500

    Added logger

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import React, { useEffect, useState } from 'react';
 import abc from "./images/actual-website.jpg";
 import styled from "styled-components";
 import ContactFormPopup from "./components/ContactFormPopup";
+import { Logger } from 'aws-amplify';
+
+const logger = new Logger('MyLoggerName');
 
 function App() {
   const [showPopup, setShowPopup] = useState(false);
   
   useEffect(() => {
+    console.log("console log - USE EFFECT IN EFFECT")
+    
+    logger.info('INFO info bar');
+    logger.debug('DEBUG debug bar');
+    logger.warn('WARN warn bar');
+    logger.error('ERROR error bar');
+    
     setTimeout(() => {
+      console.log("console log - TIMEOUT COMPLETE")
       setShowPopup(true);
     }, 750)
   }, [showPopup]);
