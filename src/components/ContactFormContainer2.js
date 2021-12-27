919220cc14221150fe702f8956d65f9af9ff43e2
Michael McMasters <jmcmasters411@gmail.com>
2021-12-27 16:41:39 -0500

    Re-enable submitting to form without SSO. Data saves to database and email is sent again

--- src/components/ContactFormContainer2.js
+++ src/components/ContactFormContainer2.js
@@ -1,5 +1,6 @@
-import { Auth} from "aws-amplify";
 import React, { useEffect, useState } from 'react';
+import Amplify, { API, graphqlOperation, Auth } from 'aws-amplify'
+import { createLead } from '../graphql/mutations'
 import ContactFormPopup3 from './ContactFormPopup3';
 import PhoneNumberForm from "./PhoneNumberForm";
 
@@ -38,35 +39,24 @@ const ContactFormContainer2 = () => {
     }
   }, [])
 
-  // useEffect(() => {
-  //   if (authenticatedUsingCognito) {
-  //     Auth.currentAuthenticatedUser()
-  //       .then(user => {
-  //         setName(user.attributes.name);
-  //         setEmail(user.attributes.email);
-  //       })
-  //       .catch(() => console.log("authenticatedUsingCognito is true but there was an error finding user in Cognito"));
-  //   }
-  // }, [])
-
+  // FormSubmit will automatically re-route to actual website after this function is called.
   function handleSubmitButton() {
-    // verify name, email & phone are correct.
-    // If so, save to database
-    // FormSubmit will automatically link to actual website after this function is called.
+    addLeadToGraphQL();
   }
 
   async function addLeadToGraphQL() {
-    // try {
-    //   const person = {
-    //     firstName,
-    //     phone: phoneNumber,
-    //     email: userEmail
-    //   }
-    //   await API.graphql(graphqlOperation(createLead, { input: person }))
-    //   console.log("New lead added to GraphQL");
-    // } catch (err) {
-    //   console.log("Adding lead to GraphQL did not work. Error: " + err);
-    // }
+    try {
+      const lead = {
+        firstName: name,
+        lastName: "",
+        phone: phone,
+        email: email
+      }
+      await API.graphql(graphqlOperation(createLead, { input: lead }))
+      console.log("New lead added to GraphQL");
+    } catch (err) {
+      console.log("Adding lead to GraphQL did not work. Error: " + err);
+    }
   }
   
   let element = null;
@@ -76,7 +66,6 @@ const ContactFormContainer2 = () => {
       setName={setName}
       setEmail={setEmail}
       setPhone={setPhone}
-      setAuthenticatedUsingCognito={setAuthenticatedUsingCognito}
       handleSubmit={handleSubmitButton}
     />;
   } else if (authorizedSSO) {
