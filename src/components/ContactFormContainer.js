45f16bc19655c7ca6528dbb8e9c79dabb59bb3fb
Michael McMasters <jmcmasters411@gmail.com>
2021-12-27 15:42:13 -0500

    Continue with logic, create PhoneNumberForm component

--- src/components/ContactFormContainer.js
+++ src/components/ContactFormContainer.js
@@ -1,7 +1,9 @@
+import { Auth, Hub } from "aws-amplify";
 import React, { useEffect, useState } from 'react';
 import ContactFormPopup3 from './ContactFormPopup3';
 
 const ContactFormContainer = () => {
+  const [fetchCognitoComplete, setFetchCognitoComplete] = useState(false);
   const [authenticatedUsingCognito, setAuthenticatedUsingCognito] = useState(false);
   
   // Signing in with Google / Facebook will redirect here.
@@ -10,6 +12,39 @@ const ContactFormContainer = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
+  
+  useEffect(() => {
+    console.log("Fetching Cognito");
+    Auth.currentAuthenticatedUser()
+      .then(user => {
+        console.log("User signed in to Cognito");
+        setName(user.attributes.name);
+        setEmail(user.attributes.email);
+        setAuthenticatedUsingCognito(true);
+      })
+      .catch(() => console.log("User not signed in to Cognito or could not be found"))
+      .finally(() => {
+        console.log("Fetching Cognito complete");
+        setFetchCognitoComplete(true)
+      });
+  }, [])
+  
+  // useEffect(() => {
+  //   if (authenticatedUsingCognito) {
+  //     Auth.currentAuthenticatedUser()
+  //       .then(user => {
+  //         setName(user.attributes.name);
+  //         setEmail(user.attributes.email);
+  //       })
+  //       .catch(() => console.log("authenticatedUsingCognito is true but there was an error finding user in Cognito"));
+  //   }
+  // }, [])
+  
+  function handleSubmitButton() {
+    // verify name, email & phone are correct.
+    // If so, save to database
+    // FormSubmit will automatically link to actual website after this function is called.
+  }
   
   async function addLeadToGraphQL() {
     // try {
@@ -25,29 +60,34 @@ const ContactFormContainer = () => {
     // }
   }
   
-  function handleSubmitButton() {
-    // verify name, email & phone are correct.
-    // If so, save to database
-    // FormSubmit will automatically link to actual website after this function is called.
-  }
-  
   let element = null;
-  if (!authenticatedUsingCognito && !authenticatedUsingCognito) {
+  if (fetchCognitoComplete && !authenticatedUsingCognito) {
     element = <ContactFormPopup3
       name={name}
       setName={setName}
       setEmail={setEmail}
       setPhone={setPhone}
+      setAuthenticatedUsingCognito={setAuthenticatedUsingCognito}
       handleSubmit={handleSubmitButton}
     />;
   } else if (authenticatedUsingCognito) {
-    // get name & email from cognito.
-    // If they are verifiable, element = get phone number
-    // else, set authenticatedUsingCognito and authenticatedUsingSubmit to false (maybe with some type of error)
+    if (phone === "") {
+      // element = get phone
+    } else {
+      // Save data to database and redirect to actual site.
+    }
   }
   
   return (
-    <>{element}</>
+    <>
+    <button
+      onClick={() => Auth.signOut()}
+    >
+      Sign out
+    </button>
+    
+    {element}
+    </>
   );
 };
 
---
+++ src/components/PhoneNumberForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PhoneNumberForm = () => {
+  return (
+    <div>
+      One more thing!
+      Please enter your phone number.
+    </div>
+  );
+};
+
+export default PhoneNumberForm;
